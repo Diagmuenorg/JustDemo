0e76be1005c7641a5a540d9716855a865e9f35d4
Diagmuenorg <kezeque264242@126.com>
2021-05-16 15:45:20 +0800

    初始化项目

---
+++ app/src/main/java/com/home/dab/justdemo/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.home.dab.justdemo
+
+import com.dab.just_kt.base.BaseJustActivity
+
+
+class MainActivity : BaseJustActivity() {
+    override fun setContentViewRes(): Int =R.layout.activity_main
+}
